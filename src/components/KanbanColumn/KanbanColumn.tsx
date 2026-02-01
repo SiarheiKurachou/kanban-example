d2ca6b206e0213939bcff3b03840f96a9f07b3a4
SiarheiKurachou <kurachov.sergey@gmail.com>
2026-02-01 20:29:09 +0000

    fix styles

--- src/components/KanbanColumn/KanbanColumn.tsx
+++ src/components/KanbanColumn/KanbanColumn.tsx
@@ -4,16 +4,16 @@ import KanbanCard from '../KanbanCard';
 import styles from './KanbanColumn.module.css';
 
 const KanbanColumn: React.FC<{ column: Column }> = ({ column }) => {
-  return (
-    <article className={styles.kanbanColumn} aria-label={`${column.title} column`} tabIndex={0}>
-      <h2 className={styles.columnTitle}>{column.title}</h2>
-      <div className={styles.cardsContainer} role="list">
-        {column.cards.map((card, i) => (
-          <KanbanCard key={i} card={card} />
-        ))}
-      </div>
-    </article>
-  );
+    return (
+        <article className={styles.kanbanColumn} aria-label={`${column.title} column`} tabIndex={0}>
+            <div className={styles.columnTitle}><h2 className={styles.columnTitlePill}>{column.title}</h2></div>
+            <div className={styles.cardsContainer} role="list">
+                {column.cards.map((card, i) => (
+                    <KanbanCard key={i} card={card} />
+                ))}
+            </div>
+        </article>
+    );
 };
 
 export default KanbanColumn;
