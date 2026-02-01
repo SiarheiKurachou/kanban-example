51712dcda626792454af72f089eeaf03e8e13008
SiarheiKurachou <kurachov.sergey@gmail.com>
2026-02-01 19:39:11 +0000

    clean-up

--- src/components/Avatar.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import styles from './Avatar.module.css';
-
-const Avatar: React.FC<{ children?: React.ReactNode }> = ({ children }) => {
-  return <span className={styles.avatar}>{children}</span>;
-};
-
-export default Avatar;
--- src/components/Tag.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import styles from './Tag.module.css';
-
-const Tag: React.FC<{ children?: React.ReactNode }> = ({ children }) => {
-  return <span className={styles.tag}>{children}</span>;
-};
-
-export default Tag;
